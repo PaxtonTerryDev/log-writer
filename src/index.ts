e22d20c617b263bbdd6cdcb35a8e1282b1314423
Paxton Terry <117777648+PaxtonTerryDev@users.noreply.github.com>
2025-07-09 16:05:00 -0600

    Adds new transports to exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export { LogWriter } from './core/logwriter';
-export { LogLevel, LogOptions, Transport, LoggerConfig, FileConfig, TransportConfig, ColorConfig, LevelFilter } from './core/interfaces';
+export { LogLevel, LogOptions, Transport, LoggerConfig, FileConfig, TransportConfig, ColorConfig, LevelFilter, LogTransportArchiveConfig } from './core/interfaces';
 export { ColorUtils } from './utils/colors';
 export { ConfigLoader } from './core/config-loader';
 export { ConsoleTransport } from './transports/console';
 export { FileTransport } from './transports/file';
 export { JSONTransport } from './transports/json';
+export { LogTransport, LogTransportOptions, RotationMethod } from './transports/log';
